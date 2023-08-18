cb01e95685a1f54ba03278e7d27d8340beddb8dd
M ö b i u s <123084362+mobius-e1@users.noreply.github.com>
2023-08-18 18:36:06 +0600

    Delete frontend directory

--- frontend/js/config.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = "http://localhost:3000/api";
-export const GET_CLIENTS_PATH = `${BASE_URL}/clients`;
-export const get_client_by_id_path = (id) => `${BASE_URL}/clients/${id}`;
-export const CREATE_CLIENT_PATH = `${BASE_URL}/clients`;
-export const get_edit_client_path = (id) => `${BASE_URL}/clients/${id}`;
-export const get_delete_client_path = (id) => `${BASE_URL}/clients/${id}`;
